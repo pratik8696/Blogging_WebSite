4e03662fb1f7b90c4d3f79c3744f45da4a8734f1
pratik8696 <mepratikanand@gmail.com>
2021-07-25 00:50:26 +0530

    v.1.0

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/",function(req,res){
+  res.render('home',{para_one:homeStartingContent});
+});
+
 
 
 
