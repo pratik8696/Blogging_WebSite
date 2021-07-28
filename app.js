6d42deaf343ca23e0d53c5c497985896e09f9cfe
pratik8696 <mepratikanand@gmail.com>
2021-07-28 12:45:09 +0530

    final

--- app.js
+++ app.js
@@ -71,6 +71,6 @@ app.post("/compose", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(3000||process.env.PORT, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
